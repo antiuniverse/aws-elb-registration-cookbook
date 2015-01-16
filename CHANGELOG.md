97c00158f20baa6b87f8e8c9d47e6f15a90d7037
Zach Brockway <neverender@gmail.com>
2015-01-16 13:22:31 -0800

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 0.2.0
+
+Added support for creating mappings per layer in addition to per hostname.
+
+
 # 0.1.0
 
 Initial release of aws-elb-registration
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'neverender@gmail.com'
 license          'Apache 2.0'
 description      'Provides recipes to trigger AWS instance registration with (or deregistration from) one or more Elastic Load Balancers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends 'awscli'
