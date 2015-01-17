3820a460043965f78eec114e5c0c748df44af955
Zach Brockway <neverender@gmail.com>
2015-01-17 14:02:50 -0800

    Handle non-existent data bag items gracefully
    Fixes #1. Also includes some refactoring and cleanup according to rubocop.

--- libraries/common.rb
+++ libraries/common.rb
@@ -1,25 +1,57 @@
 module AwsElbRegistration
+  # TODO: Rethink configuration schema and support chef search?
   BAG_NAME = 'aws-elb-registration'
+  BAG_ITEM_MAPPING_HOSTNAMES = 'hostname_mappings'
+  BAG_ITEM_MAPPING_LAYERS = 'layer_mappings'
   WILDCARD_KEY = '_all'
 
+
   module Helpers
-    def get_mapped_elbs( hostname = node[:opsworks][:instance][:hostname], layers = node[:opsworks][:instance][:layers] )
-      hostname_mappings = data_bag_item( BAG_NAME, 'hostname_mappings' )
-      layer_mappings = data_bag_item( BAG_NAME, 'layer_mappings' )
+    # Return array of ELB names mapped to ++hostname++ and/or ++layers++.
+    def get_mapped_elbs( hostname = nil, layers = nil )
+      hostname ||= node[:opsworks][:instance][:hostname]
+      layers ||= node[:opsworks][:instance][:layers]
 
-      mapped_elbs = []
+      mapped_elbs = parse_matching_rules(
+        hostname,
+        data_bag_safe_item( BAG_NAME, BAG_ITEM_MAPPING_HOSTNAMES )
+      )
 
-      # Hostname mappings
-      mapped_elbs += hostname_mappings[WILDCARD_KEY].split( /\s*,\s*/ ) if hostname_mappings.key?( WILDCARD_KEY )
-      mapped_elbs += hostname_mappings[hostname].split( /\s*,\s*/ ) if hostname_mappings.key?( hostname )
-
-      # Layer mappings
-      mapped_elbs += layer_mappings[WILDCARD_KEY].split( /\s*,\s*/ ) if layer_mappings.key?( WILDCARD_KEY )
-      layers.each do | layer_name |
-        mapped_elbs += layer_mappings[layer_name].split( /\s*,\s*/ ) if layer_mappings.key?( layer_name )
-      end
+      mapped_elbs += parse_matching_rules(
+        layers,
+        data_bag_safe_item( BAG_NAME, BAG_ITEM_MAPPING_LAYERS )
+      )
 
       return mapped_elbs.uniq
+    end
+
+    # Non-existent items return an empty hash instead of raising an exception.
+    # The non-existence of the data bag itself will still raise an exception.
+    def data_bag_safe_item( bag_name, item )
+      if data_bag( bag_name ).include?( item )
+        return data_bag_item( bag_name, item )
+      else
+        return {}
+      end
+    end
+
+    # For each string in +needles+, check for a matching key in +haystack+.
+    # If a matching key is found, split the corresponding value's comma-separated
+    # list into an array. Concatenate all such arrays and return the result.
+    # If ++include_wildcard++ is true, ++WILDCARD_KEY++ values are included.
+    # Note: The returned array can contain duplicate values.
+    def parse_matching_rules( needles, haystack, include_wildcard = true )
+      needles = [*needles]
+
+      needles.push( WILDCARD_KEY ) if include_wildcard
+
+      matched_values = []
+
+      needles.each do | needle |
+        matched_values += haystack[needle].split( /\s*,\s*/ ) if haystack.key?( needle )
+      end
+
+      return matched_values
     end
   end
 end
--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,8 @@ name             'aws-elb-registration'
 maintainer       'Zach Brockway'
 maintainer_email 'neverender@gmail.com'
 license          'Apache 2.0'
-description      'Provides recipes to trigger AWS instance registration with (or deregistration from) one or more Elastic Load Balancers'
+description      'Provides recipes to trigger AWS instance registration with (or deregistration '\
+                 'from) one or more Elastic Load Balancers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
